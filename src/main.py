8fdcec9c079fb942a78ced737a7a2d4f0268b0a8
Andrey Vagin <a.vagin@innopolis.university>
2022-06-23 22:39:25 +0300

    cropperPipeline.py code was wrapped with method

--- src/main.py
+++ src/main.py
@@ -1,10 +1,13 @@
 from cropper.imageCropper import crop_image
+from cropper.cropperPipeline import crop_image_pipeline
 from skimage import io
 import warnings
 
 warnings.filterwarnings("ignore")
 
 image = io.imread('./docs/example.jpg')
+
 cropped = crop_image(image)
 io.imsave('./docs/example_cropped.jpg', cropped)
 
+crop_image_pipeline(image, './docs/example_pipeline.jpg')
